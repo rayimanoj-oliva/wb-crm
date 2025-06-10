4d2528361b4143945821daa0592838f826ce719f
Manoj Rayi <manoj.rayi@olivaclinic.com>
2025-06-10 18:30:07 +0530

    added new controller

--- controllers/messages_controller.py
+++ controllers/messages_controller.py
@@ -9,7 +9,7 @@ from schemas.MessageSchema import MessageRead
 
 router = APIRouter()
 
-@router.get("/messages", response_model=list[MessageRead])
+@router.get("/", response_model=list[MessageRead])
 def get_messages(db: Session = Depends(get_db)):
     messages = db.query(WhatsAppMessage).order_by(WhatsAppMessage.timestamp.desc()).all()
     return messages
